80866e14d122f30c551f35dea412b4f69160a209
David Davis <davisd50@gmail.com>
2016-03-18 08:37:12 -0400

    update for test layer

--- sparc/utils/__init__.py
+++ sparc/utils/__init__.py
@@ -1,9 +1,1 @@
-# Configuration (this package only)
-from importlib import import_module
-from sparc.configuration.zcml import Configure as SparcConfigure
-def Configure():
-    SparcConfigure([import_module(__name__)])
-def ConfigureTest():
-    SparcConfigure([import_module(__name__)])
-
 from interfaces import IReferenceAttributeCopier
--- sparc/utils/tests/test_attribute_copier.py
+++ sparc/utils/tests/test_attribute_copier.py
@@ -1,13 +1,11 @@
 import unittest
-from doctest import DocTestSuite
-from doctest import DocFileSuite
+from sparc.testing.fixture import test_suite_mixin
 
-import sparc.utils
 
-def test_suite():
-    return unittest.TestSuite((
-        DocFileSuite('attribute_copier.txt',
-                     package=sparc.utils),))
+class test_suite(test_suite_mixin):
+    package = 'sparc.utils'
+    module = 'attribute_copier'
+
 
 if __name__ == '__main__':
     unittest.main(defaultTest='test_suite')
